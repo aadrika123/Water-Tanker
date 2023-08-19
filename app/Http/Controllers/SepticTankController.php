c455b390a9e2f4bcb8e75be413ccdf62273ba2f3
bikash05 <68511641+bikash05@users.noreply.github.com>
2023-08-19 13:04:06 +0530

    Update SepticTankController.php

--- app/Http/Controllers/SepticTankController.php
+++ app/Http/Controllers/SepticTankController.php
@@ -705,7 +705,8 @@ class SepticTankController extends Controller
                 ->withToken($req->bearerToken())
                 ->post($paymentUrl . 'api/payment/generate-orderid', $reqData);
 
-            return $data = json_decode($refResponse);
+            return $refResponse;
+            $data = json_decode($refResponse);
 
             if (!$data)
                 throw new Exception("Payment Order Id Not Generate");
