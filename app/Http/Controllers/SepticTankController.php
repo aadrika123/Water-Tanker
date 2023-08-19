be0106e8101e8d717a71e76f2399e29bdbe39d05
bikash05 <68511641+bikash05@users.noreply.github.com>
2023-08-19 13:38:33 +0530

    Update SepticTankController.php

--- app/Http/Controllers/SepticTankController.php
+++ app/Http/Controllers/SepticTankController.php
@@ -710,7 +710,7 @@ class SepticTankController extends Controller
             if (!$data)
                 throw new Exception("Payment Order Id Not Generate");
             if ($data->status == false) {
-                return responseMsgs(false, $data, json_decode($refResponse), "110154", "1.0", "", 'POST', $req->deviceId ?? "");
+                return responseMsgs(true, $data, json_decode($refResponse), "110154", "1.0", "", 'POST', $req->deviceId ?? "");
             }
 
             $data->name = $mStBooking->applicant_name;
