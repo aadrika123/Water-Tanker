ec1a2b558f86528f9b08f3d5882a9d563fdb4a7f
bikash05 <68511641+bikash05@users.noreply.github.com>
2023-08-22 13:20:43 +0530

    Update constants.php

--- config/constants.php
+++ config/constants.php
@@ -7,7 +7,7 @@ return [
     "BASE_URL" => 'http://192.168.0.95:8090/',
     "ULB_LOGO_URL" => 'http://203.129.217.246:8000/',
     // "ID_GENERATE_URL" => 'http://203.129.217.246:8000/',
-    "ID_GENERATE_URL" => 'http://192.168.0.202:3000/',
+    "ID_GENERATE_URL" => 'http://192.168.0.95:8000/',
     "PARAM_ID" => '35',
     "PARAM_ST_ID" => '36',
     "WATER_TANKER_MODULE_ID"=>"11",
