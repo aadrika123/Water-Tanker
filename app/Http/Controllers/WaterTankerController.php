1d24afd624813738cf925d7e9cac0fbac7b942ca
bikash05 <68511641+bikash05@users.noreply.github.com>
2023-08-24 18:28:26 +0530

    Update WaterTankerController.php

--- app/Http/Controllers/WaterTankerController.php
+++ app/Http/Controllers/WaterTankerController.php
@@ -2019,7 +2019,6 @@ class WaterTankerController extends Controller
      */
     public function listAppliedAndCancelledApplication(Request $req)
     {
-        return $req;
         try {
             if ($req->auth['user_type'] != 'Citizen')
                 throw new Exception('Unauthorized Access !!!');
