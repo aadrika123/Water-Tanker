9a8d6bd17cdb2f6982e49fbb3eb214aee12629d3
bikash05 <68511641+bikash05@users.noreply.github.com>
2023-08-25 18:55:35 +0530

    Update constants.php

--- config/constants.php
+++ config/constants.php
@@ -4,14 +4,21 @@
  * | Relative path and name of Document Uploads
  */
 return [
+    // Live Url
     "BASE_URL" => 'http://192.168.0.202:8000/',
     "ULB_LOGO_URL" => 'http://192.168.0.202:8000/',
-    // "ID_GENERATE_URL" => 'http://203.129.217.246:8000/',
+    "PAYMENT_URL" => "http://192.168.0.202:8006/",
     "ID_GENERATE_URL" => 'http://192.168.0.202:8000/',
+
+    // Local Url
+    // "BASE_URL" => 'http://192.168.0.21:8005/',
+    // "ULB_LOGO_URL" => 'http://192.168.0.202:8001/',
+    // "PAYMENT_URL" => "http://192.168.0.202:8006/",
+    // "ID_GENERATE_URL" => 'http://192.168.0.21:8000/',
+
+
     "PARAM_ID" => '35',
     "PARAM_ST_ID" => '36',
     "WATER_TANKER_MODULE_ID"=>"11",
-    // "PAYMENT_URL" => "http://192.168.0.240:8010/",
-    "PAYMENT_URL" => "http://192.168.0.202:8006/",
     "API_KEY" => "eff41ef6-d430-4887-aa55-9fcf46c72c99"
 ];
