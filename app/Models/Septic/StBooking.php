da5f0cb0344bb7c8c03f03acbcd3f5c6cbe5df45
bikash05 <68511641+bikash05@users.noreply.github.com>
2023-09-28 17:35:02 +0530

    Update StBooking.php

--- app/Models/Septic/StBooking.php
+++ app/Models/Septic/StBooking.php
@@ -60,8 +60,9 @@ class StBooking extends Model
         return DB::table('st_bookings as stb')
                     ->leftjoin('st_drivers as sd','sd.id','=','stb.driver_id')
                     ->leftjoin('st_resources as sr','sr.id','=','stb.vehicle_id')
-                    ->leftjoin('st_capacities as sc','sc.id','=','stb.capacity_id')
-                    ->select('stb.*','wtl.location','sd.driver_name','sr.vehicle_no','sc.capacity')
+                    // ->leftjoin('st_capacities as sc','sc.id','=','stb.capacity_id')
+                    // ->select('stb.*','wtl.location','sd.driver_name','sr.vehicle_no','sc.capacity')
+                    ->select('stb.*','wtl.location','sd.driver_name','sr.vehicle_no')
                     ->join('wt_locations as wtl','wtl.id','=','stb.location_id');
     }
 
