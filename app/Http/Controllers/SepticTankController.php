ebb90db76e8d697e8b4216c940c66bb4fb074fb4
pandeyprity <pandeyprity33@gmail.com>
2024-06-18 11:17:49 +0530

    added filetr dte

--- app/Http/Controllers/SepticTankController.php
+++ app/Http/Controllers/SepticTankController.php
@@ -775,7 +775,7 @@ class SepticTankController extends Controller
                 ->get();
             $list = $list->where('ulb_id', $req->auth['ulb_id'])->values();
             if ($req->fromDate != NULL)
-                $list = $list->whereBetween('cleaning_date', [$req->fromDate, $req->toDate])->values();
+                $list = $list->whereBetween('driver_delivery_update_date_time', [$req->fromDate, $req->toDate])->values();
             $ulb = $this->_ulbs;
             $DocUpload = new DocUpload();
             $f_list = $list->map(function ($val) use ($ulb, $DocUpload) {
