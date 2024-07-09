047e90d24b652d6dadf9287b36d8c62b37764f44
pandeyprity <pandeyprity33@gmail.com>
2024-07-09 15:33:46 +0530

    sms send for septic

--- app/Http/Controllers/SepticTankController.php
+++ app/Http/Controllers/SepticTankController.php
@@ -78,23 +78,23 @@ class SepticTankController extends Controller
             $res = $mStBooking->storeBooking($req);                                                                     // Store Booking Informations
             DB::commit();
             #_Whatsaap Message
-            if (strlen($req->mobile) == 10) {
+            // if (strlen($req->mobile) == 10) {
 
-                $whatsapp2 = (Whatsapp_Send(
-                    $req->mobile,
-                    "wt_booking_initiated",
-                    [
-                        "content_type" => "text",
-                        [
-                            $req->applicantName ?? "",
-                            $req->paymentAmount,
-                            "septic tanker",
-                            $req->bookingNo,
-                            "87787878787 "
-                        ]
-                    ]
-                ));
-            }
+            //     $whatsapp2 = (Whatsapp_Send(
+            //         $req->mobile,
+            //         "wt_booking_initiated",
+            //         [
+            //             "content_type" => "text",
+            //             [
+            //                 $req->applicantName ?? "",
+            //                 $req->paymentAmount,
+            //                 "septic tanker",
+            //                 $req->bookingNo,
+            //                 "87787878787 "
+            //             ]
+            //         ]
+            //     ));
+            // }
             return responseMsgs(true, "Booking Added Successfully !!!",  $res, "110201", "1.0", responseTime(), 'POST', $req->deviceId ?? "");
         } catch (Exception $e) {
             DB::rollBack();
@@ -2058,6 +2058,24 @@ class SepticTankController extends Controller
             DB::commit();
             DB::connection("pgsql_master")->commit();
             $msg = "Payment Accepted Successfully !!!";
+            #_Whatsaap Message
+            if (strlen($booking->mobile) == 10) {
+
+                $whatsapp2 = (Whatsapp_Send(
+                    $booking->mobile,
+                    "wt_booking_initiated",
+                    [
+                        "content_type" => "text",
+                        [
+                            $booking->applicant_name ?? "",
+                            $mTransaction->paid_amount,
+                            "septic tank",
+                            $booking->booking_no,
+                            "87787878787 "
+                        ]
+                    ]
+                ));
+            }
             return responseMsgs(true, $msg, ["tranId" => $mTransaction->id, "TranNo" => $mTransaction->tran_no], '110169', 01, "", 'POST', $req->deviceId);
         } catch (Exception $e) {
             DB::rollBack();
