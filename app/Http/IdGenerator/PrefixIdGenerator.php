52d17f0db839e9125149ce102b0854b224b5d391
pandeyprity <pandeyprity33@gmail.com>
2024-08-24 16:50:32 +0530

    code correction

--- app/Http/IdGenerator/PrefixIdGenerator.php
+++ app/Http/IdGenerator/PrefixIdGenerator.php
@@ -2,9 +2,10 @@
 
 namespace App\Http\IdGenerator;
 
-use App\Models\ForeignModels\UlbMaster ;
+use App\Models\ForeignModels\UlbMaster;
 // use App\Models\IdGenerationParam ;
 use App\Models\ForeignModels\IdGenerationParam;
+use App\Models\Septic\StBooking;
 
 /**
  * | Created On-04/09/2023 
@@ -29,6 +30,40 @@ class PrefixIdGenerator implements iIdGenerator
     /**
      * | Id Generation Business Logic 
      */
+    // public function generate(): string
+    // {
+    //     $paramId = $this->paramId;
+    //     $mIdGenerationParams = new IdGenerationParam();
+    //     $mUlbMaster = new UlbMaster();
+    //     $ulbDtls = $mUlbMaster::findOrFail($this->ulbId);
+
+    //     $ulbDistrictCode = $ulbDtls->district_code;
+    //     $ulbCategory = $ulbDtls->category;
+    //     $code = $ulbDtls->code;
+
+    //     $params = $mIdGenerationParams->getParams($paramId);
+    //     $prefixString = $params->string_val;
+    //     $stringVal = $ulbDistrictCode . $ulbCategory . $code;
+
+    //     $stringSplit = collect(str_split($stringVal));
+    //     $flag = ($stringSplit->sum()) % 9;
+    //     $intVal = $params->int_val;
+    //     // Case for the Increamental
+    //     if ($this->incrementStatus == true) {
+    //         $id = $stringVal . str_pad($intVal, 7, "0", STR_PAD_LEFT);
+    //         $intVal += 1;
+    //         $params->int_val = $intVal;
+    //         $params->save();
+    //     }
+
+    //     // Case for not Increamental
+    //     if ($this->incrementStatus == false) {
+    //         $id = $stringVal  . str_pad($intVal, 7, "0", STR_PAD_LEFT);
+    //     }
+
+    //     return $prefixString . '-' . $id . $flag;
+    // }
+
     public function generate(): string
     {
         $paramId = $this->paramId;
@@ -47,19 +82,33 @@ class PrefixIdGenerator implements iIdGenerator
         $stringSplit = collect(str_split($stringVal));
         $flag = ($stringSplit->sum()) % 9;
         $intVal = $params->int_val;
-        // Case for the Increamental
-        if ($this->incrementStatus == true) {
+        $unique = false;
+
+        // Loop until a unique booking_no is generated
+        while (!$unique) {
             $id = $stringVal . str_pad($intVal, 7, "0", STR_PAD_LEFT);
-            $intVal += 1;
-            $params->int_val = $intVal;
+            $bookingNo = $prefixString . '-' . $id . $flag;
+
+            // Check if the generated bookingNo already exists
+            if (!$this->checkBookingNoExists($bookingNo)) {
+                $unique = true;
+            } else {
+                $intVal += 1; // Increment the counter if duplicate is found
+            }
+        }
+
+        // Save the updated int_val
+        if ($this->incrementStatus == true) {
+            $params->int_val = $intVal + 1; // Increment the value for the next generation
             $params->save();
         }
 
-        // Case for not Increamental
-        if ($this->incrementStatus == false) {
-            $id = $stringVal  . str_pad($intVal, 7, "0", STR_PAD_LEFT);
-        }
+        return $bookingNo;
+    }
 
-        return $prefixString . '-' . $id . $flag;
+
+    public function checkBookingNoExists($bookingNo)
+    {
+        return StBooking::where('booking_no', $bookingNo)->exists();
     }
 }
