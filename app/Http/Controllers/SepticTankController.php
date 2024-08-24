7fb2b70a5788bdb008417e10705c6ae25f6eabd0
pandeyprity <pandeyprity33@gmail.com>
2024-08-24 17:15:53 +0530

    filter

--- app/Http/Controllers/SepticTankController.php
+++ app/Http/Controllers/SepticTankController.php
@@ -833,8 +833,8 @@ class SepticTankController extends Controller
                 ->get();
             $list = $list->where('ulb_id', $req->auth['ulb_id'])->values();
             if ($req->fromDate != NULL)
-                //$list = $list->whereBetween('stb.driver_delivery_update_date_time', [$req->fromDate, $req->toDate])->values();
-                $list = $list->whereBetween(DB::raw('DATE(stb.driver_delivery_update_date_time)'), [$req->fromDate, $req->toDate])->values();
+            $list = $list->whereBetween('stb.driver_delivery_update_date_time', [$req->fromDate, $req->toDate])->values();
+                // $list = $list->whereBetween(DB::raw('DATE(stb.driver_delivery_update_date_time)'), [$req->fromDate, $req->toDate])->values();
             $ulb = $this->_ulbs;
             $DocUpload = new DocUpload();
             $f_list = $list->map(function ($val) use ($ulb, $DocUpload) {
